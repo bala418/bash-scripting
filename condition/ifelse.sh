a1a548be5bc2a1a15516199cb06dbdabda5f6652
bala418 <balavtwo@gmail.com>
2022-05-22 19:35:36 +0530

    2 condition if

--- condition/ifelse.sh
+++ condition/ifelse.sh
@@ -1,7 +1,6 @@
 # reading number in program
 echo -n "Enter a number : "
-read num
-
+num=9
 
 if [ $num -eq 0 ]
 then
@@ -21,3 +20,50 @@ else
 fi
 
 echo "$1 is entered as an argument while running the command"
+
+
+# double condition checking
+
+age=20
+if [ $age -gt 18 ] && [ $age -lt 30 ]
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
+
+
+if [ $age -gt 18 -a $age -lt 30 ]
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
+
+if((age>18 && age<30))
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
+
+if([ $age -gt 18 ] && [ $age -lt 30 ])
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
+
+if [[ $age -gt 18 && $age -lt 30 ]]
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
+
+if [[ $age -gt 18 && $age -lt 30 ]]
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
