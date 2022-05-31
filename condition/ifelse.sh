d45b508a109e643662cc613f4ccf879898539c89
bala418 <balavtwo@gmail.com>
2022-05-31 21:54:46 +0530

    Update condition

--- condition/ifelse.sh
+++ condition/ifelse.sh
@@ -1,8 +1,8 @@
 # reading number in program
-echo -n "Enter a number : "
-num=9
+echo -n "Enter a number: "
+read num
 
-if [ $num -eq 0 ]
+if [[ $num -eq 0 ]]
 then
 	echo "The number is zero"
 elif [ $num -gt 0 ]
@@ -12,6 +12,20 @@ else
 	echo "The number is negative"
 fi
 
+
+if [ "$num" -eq 0 ]
+then
+	echo "The number is zero"
+elif [ "$num" -gt 0 ]
+then
+	echo "The number is positive"
+else
+	echo "The number is negative"
+fi
+
+# if number is divisible by 2 then print even
+
+
 if [ $((num%2)) -eq 0 ]
 then
 	echo "$num is even"
@@ -19,8 +33,68 @@ else
 	echo "$num is odd"
 fi
 
-echo "$1 is entered as an argument while running the command"
+if ((num>0))
+then
+	echo "$num is positive"
+elif ((num<0))
+then
+	echo "$num is negative"
+else
+	echo "$num is zero"
+fi
 
+
+if (($num>0))
+then
+	echo "$num is positive"
+elif (($num<0))
+then
+	echo "$num is negative"
+else
+	echo "$num is zero"
+fi
+
+# check if num + 2 is even
+if [[ $((num+2))%2 -eq 0 ]]
+then
+	echo "$((num+2)) is even"
+else
+	echo "$((num+2)) is odd"
+fi
+
+# c style if else
+if ((($num+2)%2))
+then
+	echo "$((num+2)) is odd"
+else
+	echo "$((num+2)) is even"
+fi
+
+# increment num by 2
+if [ $((num+2)) -eq 11 ]
+then
+	echo "$num + 2 is 11"
+else
+	echo "$num + 2 is not 11"
+fi
+
+# if num+2 is even then print even
+if [ `expr $((num+2)) % 2` -eq 0 ]
+then
+	echo "$((num+2)) is even"
+else
+	echo "$((num+2)) is odd"
+fi
+
+if [[ $num == 0 ]]
+then
+	echo "$num is zero"
+elif [ $num > 0 ]
+then
+	echo "$num is positive"
+else
+	echo "$num is negative"
+fi
 
 # double condition checking
 
@@ -40,21 +114,14 @@ else
 	echo "You are not eligible for voting"
 fi
 
-if((age>18 && age<30))
+if ((age>18 && age<30))
 then
-	echo "You are eligible for voting"
+	echo "You are eligible for vffoting"
 else
 	echo "You are not eligible for voting"
 fi
 
-if([ $age -gt 18 ] && [ $age -lt 30 ])
-then
-	echo "You are eligible for voting"
-else
-	echo "You are not eligible for voting"
-fi
-
-if [[ $age -gt 18 && $age -lt 30 ]]
+if ([ $age -gt 18 ] && [ $age -lt 30 ])
 then
 	echo "You are eligible for voting"
 else
@@ -66,4 +133,19 @@ then
 	echo "You are eligible for voting"
 else
 	echo "You are not eligible for voting"
+fi
+
+if ([ $age -gt 18 -a $age -lt 30 ])
+then
+	echo "You are eligible for voting"
+else
+	echo "You are not eligible for voting"
+fi
+
+
+
+i=7
+
+if [ "$i" -ne 6 ] && [ "$i" -ne 8 ]; then
+   echo 'i is neither 6 nor 8'
 fi
--- condition/switch.sh
+++ condition/switch.sh
@@ -1,5 +1,6 @@
 # switch case
 
+# number
 echo -n "Enter a number : "
 read n
 case $n in
@@ -16,10 +17,81 @@ case $n in
     *) echo "Invalid number";;
 esac
 
+# string
 echo -n "Enter a string : "
 read str
 case $str in
     "Hello") echo "Hello World";;
     "Bye") echo "Bye World";;
-    *) echo "Invalid string";;
+    *) echo "Invalid string not hello or bye";;
+esac
+
+
+# arithmetic operators
+echo -n "Enter 2 numbers : "
+read a b
+echo -n "Enter the operator : "
+read op
+case $op in
+    "+") echo "Sum is $((a+b))";;
+    "-") echo "Difference is $((a-b))";;
+    "*") echo "Product is $((a*b))";;
+    "/") echo "Quotient is $((a/b))";;
+    "%") echo "Remainder is $((a%b))";;
+    *) echo "Invalid operator";;
+esac
+
+# relational operators
+echo -n "Enter the relational operator : "
+read op
+case $op in
+    "==")
+        if [ $a -eq $b ]
+        then
+            echo "True"
+        else
+            echo "False"
+        fi
+        ;;
+    "!=")
+        if [ $a -ne $b ]
+        then
+            echo "True"
+        else
+            echo "False"
+        fi
+        ;;
+    ">")
+        if [ $a -gt $b ]
+        then
+            echo "True"
+        else
+            echo "False"
+        fi
+        ;;
+    ">=")
+        if [ $a -ge $b ]
+        then
+            echo "True"
+        else
+            echo "False"
+        fi
+        ;;
+    "<")
+        if [ $a -lt $b ]
+        then
+            echo "True"
+        else
+            echo "False"
+        fi
+        ;;
+    "<=")
+        if [ $a -le $b ]
+        then
+            echo "True"
+        else
+            echo "False"
+        fi
+        ;;
+    *) echo "Invalid operator";;
 esac
